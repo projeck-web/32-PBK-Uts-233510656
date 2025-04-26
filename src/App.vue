c211db760e5e1ecf16f452cdb75d0d9ac74c85f2
projeck-web <larasgann@Gmail.com>
2025-04-26 12:44:34 +0700

    Menambahkan Variable input dan CheckBox

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const newTask = ref('')
+const showOnlyIncomplete = ref(false)
+
+
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>

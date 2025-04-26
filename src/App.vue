dd1899bfc13e4cdb9040f65af6d2ddefffad401f
projeck-web <larasgann@Gmail.com>
2025-04-26 12:52:25 +0700

    Tempat Untuk Menyimpan Daftar Kegiatan Berbentuk Array

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ import { ref, computed } from 'vue'
 
 const newTask = ref('')
 const showOnlyIncomplete = ref(false)
-
+const tasks = ref([
+  { text: 'berkebun', done: false },
+  { text: 'belajar javascript', done: false },
+  { text: 'mengerjakan tugas', done: false }
+])
 
 </script>
 
